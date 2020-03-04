0f74c1a2c67b63401bfb8d50f614587376565b91
Bhavik Mehta <bhavikmehta8@gmail.com>
2020-03-04 16:13:15 +0000

    fix over

--- src/to_mathlib.lean
+++ src/to_mathlib.lean
@@ -14,13 +14,13 @@ variables {C : Type u₁} [𝒞 : category.{v₁} C]
 include 𝒞
 
 lemma mono_comp_of_mono {X Y Z : C}
-  (m : X ⟶ Y) (m' : Y ⟶ Z) [mono m] [mono m'] : mono (m ≫ m') :=
+  (m : X ⟶ Y) (m' : Y ⟶ Z) (hm : mono m) (hm' : mono m') : mono (m ≫ m') :=
 ⟨λ Z f g w,
   have f ≫ m = g ≫ m := (cancel_mono m').mp (by simp only [category.assoc]; exact w),
   (cancel_mono m).mp this⟩
 
 lemma epi_comp_of_epi {X Y Z : C}
-  (e : X ⟶ Y) (e' : Y ⟶ Z) [epi e] [epi e'] : epi (e ≫ e') :=
+  (e : X ⟶ Y) (e' : Y ⟶ Z) (he : epi e) (he' : epi e') : epi (e ≫ e') :=
 ⟨λ Z f g w,
   have e' ≫ f = e' ≫ g := (cancel_epi e).mp (by simp only [category.assoc] at w; exact w),
   (cancel_epi e').mp this⟩
