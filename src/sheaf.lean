2deae5e38a310da34f9450f7a1084c40661dfa94
Bhavik Mehta <bhavikmehta8@gmail.com>
2020-12-16 23:49:04 +0000

    yay

--- src/sheaf.lean
+++ src/sheaf.lean
@@ -520,28 +520,16 @@ def closed_class_equiv {B B' : C} (m : B' ⟶ B) [closure.dense j m] :
 lemma closed_class_equiv_forward {B B' : C} (m : B' ⟶ B) [closure.dense j m] (f : B ⟶ closed_classifier j) :
   m ≫ f = closed_class_equiv j m f :=
 begin
-  sorry
-  -- dsimp [closed_class_equiv, eq_equiv, closed_equiv, action, closure_equiv, equiv.subtype_congr],
-  -- ext1,
-  -- rw equalizer.lift_ι,
-  -- change _ = quotient.lift _ _ (quotient.mk _),
-  -- dsimp,
-  -- change _ = classifier_of ((sub.pullback m).obj (sub.mk' (get_subobject (f ≫ equalizer.ι j (𝟙 (Ω C)))))),
-
-  -- change _ = classify ((subq.pullback m).obj ⟦_⟧),
-  -- sorry,
-  -- dsimp [cl]
-  -- change _ = classify _,
-  -- rw classify_pullback,
-
-  -- simp [closed_class_equiv, eq_equiv, closed_equiv, action, closure_equiv, equiv.subtype_congr],
-  -- ext1,
-  -- rw equalizer.lift_ι,
-  -- change _ = classify _,
-  -- rw classify_pullback,
-  -- change _ = m ≫ classification.symm (classification _),
-  -- rw classification.symm_apply_apply,
-  -- rw [assoc], refl,
+  dsimp [closed_class_equiv, eq_equiv, closed_equiv, action, closure_equiv, equiv.subtype_congr],
+  ext1,
+  rw equalizer.lift_ι,
+  -- dsimp [subq.pullback, lower_sub],
+  change _ = classify ((subq.pullback m).obj ⟦_⟧),
+  -- change _ = classifier_of _,
+  rw classify_pullback,
+  change _ = m ≫ classification.symm (classification _),
+  rw classification.symm_apply_apply,
+  rw assoc,
 end
 
 def sheaf_classifier : sheaf j :=
